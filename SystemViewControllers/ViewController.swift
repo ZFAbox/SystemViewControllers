2c3c14fc9fd291af65ecd60a128c4b84fdde2fd5
Fedor <starbreeze@yandex.ru>
2025-07-10 00:35:08 +0300

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Федор Завьялов on 10.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func shareButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: Any) {
+    }
+}
+
